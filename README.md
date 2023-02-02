f8fe2e75605cd09ffdc6aa8cde5f06e9a8f7c461
Javier Orfo <javierorfo@protonmail.com>
2023-02-01 21:33:30 -0300

    xmonarch

--- README.md
+++ README.md
@@ -1,1 +1,192 @@
-# xmonarch
+# XMonarch
+*XMonarch is a personal configuration to get **XMonad** and **XMobar** in one piece on **Arch Linux**. This applies perfectly to my personal desktop computer, my personal notebook and my notebook for working with no need to have multiple file configurations for each one.*
+
+## Caveats
+- This set of tools have my own personal configurations (with `Nerd Fonts`, `Alacritty`, etc), feel free to use it and modify it as you wish.
+- This project has been developed on and for Linux following open source philosophy.
+
+## Installation
+```console
+$ git clone https://github.com/javi-7/xmonarch
+$ cd xmonarch
+$ ./xmonarch
+```
+- `./xmonarch` will install **Ubuntu Mono Nerd Font** (if not installed), set **00-keyboard.conf** with LATAM distribution (if you like), and install xmonad and settings (and remove older configurations).
+
+## Configuration
+- Set preferences in [Settings.hs](https://github.com/javi-7/xmonarch/blob/master/xmonad/lib/XMonad/Util/Settings.hs)
+```haskell
+-- __  ____  __                            _
+-- \ \/ /  \/  | ___  _ __   __ _ _ __ ___| |__
+--  \  /| |\/| |/ _ \| '_ \ / _` | '__/ __| '_ \
+--  /  \| |  | | (_) | | | | (_| | | | (__| | | |
+-- /_/\_\_|  |_|\___/|_| |_|\__,_|_|  \___|_| |_|
+
+-- #############
+-- # CHANGE ME #
+-- #############
+
+-- TERMINAL
+terminal                     = "alacritty"
+
+-- XMOBAR COLORS
+xmobarBgColor                = "#303030"
+xmobarFgColor                = "#FFFFFF"
+xmobarWSCurrentColor         = "#FF0000"
+xmobarWSHiddenColor          = "#00FFFF"
+xmobarWSHiddenNoWindowsColor = "#FFFFFF"
+xmobarWSTitleColor           = "#FFFFFF"
+xmobarArchLogoColor          = "#1793D1"
+
+-- XMOBAR SEPARATOR
+xmobarSep                    = " <fc=#949494>|</fc> "
+
+-- XMOBAR POSITION
+xmobarPosition               = "TopH 25"
+
+-- XMONAD WINDOWS
+borderWidth                  = 2
+normalBorderColor            = "#DDDDDD"
+focusedBorderColor           = "#FF0000"
+
+-- NERD FONT
+fontDefault                  = "UbuntuMono Nerd Font Mono"
+fontFirst                    = fontDefault
+fontSecond                   = fontDefault
+bold                         = False
+fontSize                     = 11
+
+-- XMOBAR COMMANDS
+-- KBD
+kbdVisible                   = True
+kbdIcon                      = "<fn=2>\\xf11c</fn>"
+
+-- DYN NETWORK
+dynNetworkVisible            = True
+dynNetworkUpIcon             = "<fn=1>\\xf0aa</fn>"
+dynNetworkDownIcon           = "<fn=1>\\xf0ab</fn>"
+
+-- DISK U
+diskUVisible                 = True
+diskUIcon                    = "<fn=1>\\xf7c9</fn>"
+
+-- DATE
+dateZoneVisible              = True
+dateZoneLocale               = "es_AR.UTF-8"
+dateZoneTimeZone             = "America/Argentina/Buenos_Aires"
+dateZoneCalendarIcon         = "<fn=1>\\xf133</fn>"
+dateZoneClockIcon            = "<fn=1>\\xf017</fn>"
+
+-- WEATHER
+weatherVisible               = True
+weatherStationID             = "SADF"
+weatherIcon                  = "<fc=#FFFF5F><fn=1>\\xe30c</fn></fc>"
+weatherHumidityIcon          = "<fc=#00FFFF>\\xe373</fc>"
+
+-- VOLUME
+volumeVisible                = True
+volumeOnIcon                 = "<fc=#FFFFFF><fn=1>\\xfa7d</fn></fc>"
+volumeOffIcon                = "<fc=red><fn=1>\\xfa80</fn></fc>"
+
+-- MULTI CPU
+multiCpuVisible              = True
+multiCpuIcon                 = "<fn=1>\\xf878</fn>"
+multiCpuLowColor             = "#FFFFFF"
+multiCpuNormalColor          = multiCpuLowColor
+multiCpuHighColor            = "red"
+
+-- MULTI CORE TEMP
+multiCoreTempVisible         = True
+multiCoreTempIcon            = "<fn=1>\\xf8c7</fn>"
+multiCoreTempLowColor        = "#FFFFFF"
+multiCoreTempNormalColor     = multiCoreTempLowColor
+multiCoreTempHighColor       = "red"
+
+-- MEMORY
+memoryVisible                = True
+memoryIcon                   = "<fn=1>\\xf85a</fn>"
+memoryLowColor               = "#FFFFFF"
+memoryNormalColor            = memoryLowColor
+memoryHighColor              = "red"
+
+-- BATTERY
+batteryVisible               = False
+batteryOnIcon                = "\\xf583"
+batteryOffIcon               = "\\xf580"
+batteryIdleIcon              = "\\xf578"
+batteryLowColor              = "red"
+batteryNormalColor           = "#FFFFFF"
+batteryHighColor             = batteryNormalColor
+--
+
+-- WORKSPACES
+workspaces                   = [ " <fn=1>\xe62b</fn> "
+                               , " <fn=1>\xfa9e</fn> "
+                               , " <fn=1>\xf1c0</fn> "
+                               , " <fn=1>\xf724</fn> "
+                               , " <fn=1>\xf075</fn> "
+                               , " <fn=1>\xf001</fn> "
+                               ]
+
+-- EXECUTE AT START
+spawns                       = [ "xset s off"
+                               , "xset -dpms"
+                               , "xset s noblank"
+                               , "feh --bg-scale ~/.config/xmonad/images/background.jpg | xautolock -time 30 -locker 'systemctl suspend' &"
+                               ]
+
+-- XMONAD KEY BINDINGS
+volumeKeyBound               = True
+brightKeyBound               = False
+
+-- ###################
+-- # END OF SETTINGS #
+-- ###################
+
+```
+
+## Overview
+- [x] XMonad in Settings.hs
+  - [x] Terminal
+  - [x] Windows borders
+  - [x] Nerd Fonts configuration
+  - [x] Workspaces configuration
+  - [x] Initial spawns configuration
+  - [x] Volume keymap configuration
+  - [x] Brightness keymap configuration
+  - [ ] Keymaps configuration
+  - [ ] Hooks configuration
+  - [ ] Spacing configuration
+- [x] XMobar in Settings.hs
+  - [x] Colors
+  - [x] Position
+  - [x] Separator
+  - [x] Nerd Fonts configuration
+  - [x] KBD style and optional
+  - [x] DYN NETWORK style and optional
+  - [x] DISK U style and optional
+  - [x] DATE configuration, style and optional
+  - [x] WEATHER configuration, style and optional
+  - [x] VOLUME style and optional
+  - [x] MULTI CPU style and optional
+  - [x] MULTI CORE TEMP style and optional
+  - [x] MEMORY style and optional
+  - [x] BATTERY configuration, style and optional
+  - [ ] BLUETOOTH
+
+
+## Screenshots
+### Desktop Computer
+
+<img src="https://github.com/javi-7/img/raw/branch/master/xmonarch/xmonarch.png" alt="xmonarch pc" style="width:1200px;"/>
+
+### Notebook
+
+<img src="https://github.com/javi-7/img/raw/branch/master/xmonarch/xmonarch-note.png" alt="xmonarch note" style="width:1200px;"/>
+
+### Notebook 2
+
+<img src="https://github.com/javi-7/img/raw/branch/master/xmonarch/xmonarch-note2.png" alt="xmonarch note" style="width:1200px;"/>
+
+## Support
+- [Paypal](https://www.paypal.com/donate/?hosted_button_id=DT5ZGHRJKYJ8C)
---
+++ xmonad/xmonad.hs
@@ -0,0 +1,146 @@
+import XMonad
+import System.Exit
+import qualified XMonad.StackSet as W
+import Data.List
+import Data.Monoid
+import qualified Data.Map as M
+import System.IO (hPutStrLn)
+import XMonad.Util.Run
+import XMonad.Util.SpawnOnce
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import Graphics.X11.ExtraTypes.XF86
+import XMonad.Actions.WithAll (killAll)
+import XMonad.Prompt (XPPosition(Top), alwaysHighlight, font, position, promptBorderWidth)
+import XMonad.Prompt.Shell (shellPrompt)
+import qualified XMonad.Util.Settings as S
+
+myPrompt = def
+  { position          = Top
+  , alwaysHighlight   = True
+  , promptBorderWidth = 0
+  , font              = S.xmobarDefaultFont
+  }
+
+myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
+
+    [ ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
+    , ((modm,               xK_p     ), shellPrompt myPrompt)
+    , ((modm .|. shiftMask, xK_c     ), kill)
+
+     -- Rotate through the available layout algorithms
+    , ((modm,               xK_space ), sendMessage NextLayout)
+
+    --  Reset the layouts on the current workspace to default
+    , ((modm .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf)
+
+    -- Resize viewed windows to the correct size
+    , ((modm,               xK_n     ), refresh)
+    , ((modm,               xK_Tab   ), windows W.focusDown)
+    , ((modm,               xK_j     ), windows W.focusDown)
+    , ((modm,               xK_k     ), windows W.focusUp  )
+    , ((modm,               xK_m     ), windows W.focusMaster  )
+
+    -- Swap the focused window and the master window
+    , ((modm,               xK_Return), windows W.swapMaster)
+
+    -- Swap the focused window with the next window
+    , ((modm .|. shiftMask, xK_j     ), windows W.swapDown  )
+
+    -- Swap the focused window with the previous window
+    , ((modm .|. shiftMask, xK_k     ), windows W.swapUp    )
+    , ((modm,               xK_h     ), sendMessage Shrink)
+    , ((modm,               xK_l     ), sendMessage Expand)
+
+    -- Push window back into tiling
+    , ((modm,               xK_t     ), withFocused $ windows . W.sink)
+
+    -- Increment the number of windows in the master area
+    , ((modm              , xK_comma ), sendMessage (IncMasterN 1))
+
+    -- Deincrement the number of windows in the master area
+    , ((modm              , xK_period), sendMessage (IncMasterN (-1)))
+    , ((modm .|. shiftMask, xK_q     ), io (exitWith ExitSuccess))
+    , ((modm .|. shiftMask, xK_a     ), killAll)
+    , ((modm              , xK_q     ), spawn "xmonad --recompile; xmonad --restart")
+    , ((modm .|. shiftMask, xK_s     ), spawn "shutdown now")
+    , ((modm .|. shiftMask, xK_b     ), spawn "brave")
+    , ((modm .|. shiftMask, xK_t     ), spawn "thunar")
+    ]
+    ++
+
+    [((m .|. modm, k), windows $ f i)
+        | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
+        , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
+    ++
+
+    [((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
+        | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
+        , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
+    ++
+
+    if S.volumeKeyBound then [ ((0                 , xF86XK_AudioMute),        spawn "pamixer -t")
+                             , ((0                 , xF86XK_AudioLowerVolume), spawn "pamixer -d 5")
+                             , ((0                 , xF86XK_AudioRaiseVolume), spawn "pamixer -i 5")
+                             ] else []
+    ++
+
+    if S.brightKeyBound then [ ((0                 , xF86XK_MonBrightnessUp),   spawn "lux -a 10%")
+                             , ((0                 , xF86XK_MonBrightnessDown), spawn "lux -s 10%")
+                             ] else []
+
+
+
+
+myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
+    [ ((modm, button1), (\w -> focus w >> mouseMoveWindow w
+                                       >> windows W.shiftMaster))
+    , ((modm, button2), (\w -> focus w >> windows W.shiftMaster))
+    , ((modm, button3), (\w -> focus w >> mouseResizeWindow w
+                                       >> windows W.shiftMaster))
+    ]
+
+xmobarConf = "xmobar -f '" ++ S.xmobarDefaultFont ++ ":antialias=true' \
+                \ -N '" ++ S.xmobarFirstFont ++ "' -N '" ++ S.xmobarSecondFont ++ "' \
+                \ -B '" ++ S.xmobarBgColor ++ "' \
+                \ -F '" ++ S.xmobarFgColor ++ "' \
+                \ -p '" ++ S.xmobarPosition ++ "' \
+                \ -t '" ++ S.template ++ "' \
+                \ -C '[Run UnsafeStdinReader]' " ++ S.commands ++ " \
+                \ -C '[Run Com \"echo\"[\"<fc=" ++ S.xmobarArchLogoColor ++ "><fn=2>\\xf303</fn></fc> \"] \"archlogo\" 360000000]' \
+                \ -s '%' -a '}{' "
+
+main = do
+        xmproc <- spawnPipe xmobarConf
+        xmonad $ docks def
+            {
+             manageHook           = manageDocks <+> composeAll
+                                    [ className =? "MPlayer"        --> doFloat
+                                    , className =? "Gimp"           --> doFloat
+                                    , title     =? "Microsoft Teams Notification" --> doFloat
+                                    , resource  =? "desktop_window" --> doIgnore
+                                    , resource  =? "kdesktop"       --> doIgnore ]
+             , terminal           = S.terminal
+             , focusFollowsMouse  = False
+             , clickJustFocuses   = False
+             , borderWidth        = fromIntegral S.borderWidth
+             , modMask            = mod4Mask
+             , workspaces         = S.workspaces
+             , normalBorderColor  = S.normalBorderColor
+             , focusedBorderColor = S.focusedBorderColor
+             , keys               = myKeys
+             , mouseBindings      = myMouseBindings
+             , layoutHook         = let tiled = Tall 1 (3/100) (1/2)
+                                    in avoidStruts (tiled ||| Mirror tiled ||| Full)
+             , handleEventHook    = mempty
+             , logHook            = dynamicLogWithPP xmobarPP
+                                    { ppOutput = hPutStrLn xmproc
+                                    , ppCurrent = xmobarColor S.xmobarWSCurrentColor ""
+                                    , ppHidden = xmobarColor S.xmobarWSHiddenColor "" . wrap "\xf069" ""
+                                    , ppHiddenNoWindows = xmobarColor S.xmobarWSHiddenNoWindowsColor ""
+                                    , ppTitle = xmobarColor S.xmobarWSTitleColor "" . shorten 50
+                                    , ppOrder = \(ws:_:t:_) -> [ws]
+                                    }
+             , startupHook        = do
+                                    spawnOnce S.stringToSpawn
+            }
