33716054064b9acc27d0b11471d4ec6826ae04f5
Mr. Charkuils <charkuils@proton.me>
2023-02-12 12:50:13 -0300

    change ubuntu nerd font by ttf-ubuntu-mono-nerd

--- xmonarch.sh
+++ xmonarch.sh
@@ -1,6 +1,5 @@
 ##!/usr/bin/env bash
 # Author: Mr. Charkuils
-# Last Edited: 05 Dec 2022
 
 echo -e "\033[0;33m"
 echo -e "                                           _      "
@@ -11,26 +10,7 @@ echo -e "/_/\_\_| |_| |_|\___/|_| |_|\__,_|_|  \___|_| |_| "
 echo -e "\033[0m"
 
 # Required packages
-sudo pacman -S --needed xmonad xmonad-contrib xmobar pamixer xautolock pulseaudio-alsa feh alacritty
-
-# Check UbuntuMono Nerd Font
-if ! ls ~/.local/share/fonts/nerd-fonts/UbuntuMono* 1> /dev/null 2>&1; then
-    echo "xmonarch ==> Ubuntu Mono Nerd Font does not exist. Proceeding to install it..."
-    sleep 1
-
-    sudo pacman -S --needed wget unzip
-
-    mkdir -p ~/.local/share/fonts/nerd-fonts
-
-    # version v2.2.2. Update this when corresponds.
-    wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/UbuntuMono.zip -P ~/.local/share/fonts/nerd-fonts
-
-    unzip ~/.local/share/fonts/nerd-fonts/UbuntuMono.zip
-    rm ~/.local/share/fonts/nerd-fonts/UbuntuMono.zip
-    fc-cache -vf ~/.local/share/fonts/nerd-fonts
-
-    echo "xmonarch ==> Ubuntu Mono Nerd Font installed!"
-fi
+sudo pacman -S --needed xmonad xmonad-contrib xmobar pamixer xautolock pulseaudio-alsa feh alacritty ttf-ubuntu-mono-nerd
 
 # Function to copy xmonad
 copy_xmonarch() {
